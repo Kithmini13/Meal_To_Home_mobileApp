2b023dad91d21b888830ce92aa2d7931d17f3d04
Kithmini13 <kithminikaushalya19991113@gmail.com>
2021-08-12 21:48:23 +0530

    Text commit

---
+++ app/src/main/java/com/example/meal_to_home/MainActivity.java
@@ -0,0 +1,14 @@
+package com.example.meal_to_home;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
