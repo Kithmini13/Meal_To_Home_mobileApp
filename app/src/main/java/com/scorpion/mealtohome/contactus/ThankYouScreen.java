59472f908957bd778647025d4ef28e6a7b30a4ff
Nuwanthika Samarawila <nuwanthikasamarawila@gmail.com>
2021-08-13 23:00:29 +0530

    contact us complete

--- app/src/main/java/com/scorpion/mealtohome/contactus/ThankYouScreen.java
+++ app/src/main/java/com/scorpion/mealtohome/contactus/ThankYouScreen.java
@@ -2,24 +2,15 @@ package com.scorpion.mealtohome.contactus;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.app.Activity;
-import android.graphics.Color;
 import android.os.Bundle;
-import android.view.View;
-import android.widget.Toast;
 
 import com.scorpion.mealtohome.R;
 
-
-public class ContactUs extends AppCompatActivity {
+public class ThankYouScreen extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_contact_us);
-    }
-
-
-   public void submitForm(View v){
+        setContentView(R.layout.activity_thank_you_screen);
     }
 }
