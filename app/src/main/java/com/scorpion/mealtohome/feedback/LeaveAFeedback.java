4a8ae178447c6dc160b92d7b65e6397caa2c8034
Nuwanthika Samarawila <nuwanthikasamarawila@gmail.com>
2021-08-13 11:25:23 +0530

    initial feedback package

---
+++ app/src/main/java/com/scorpion/mealtohome/feedback/LeaveAFeedback.java
@@ -0,0 +1,16 @@
+package com.scorpion.mealtohome.feedback;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.scorpion.mealtohome.R;
+
+public class LeaveAFeedback extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_leave_afeedback);
+    }
+}
